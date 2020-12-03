bf207992dc2e1aad228621c7a2ee235cca970d78
Adrian Ciura <aciura@gmail.com>
2020-12-03 20:21:14 +0100

    Handling png images with next-images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withImages = require('next-images')
+
+module.exports = withImages({
+  webpack(config, options) {
+    return config
+  },
   images: {
     domains: ['test.justjoin.it'],
   },
-}
+})
