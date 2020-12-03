4e63ffa87884e54dbd0043fadbe17dfd24bd1cc7
Adrian Ciura <aciura@gmail.com>
2020-12-03 20:22:42 +0100

    Fix for Leaflet marker-icon not displaying

--- components/Map.js
+++ components/Map.js
@@ -1,17 +1,29 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 
-const Map = () => {
+delete L.Icon.Default.prototype._getIconUrl
+
+// Known issue with Leaflet & webpack: https://github.com/PaulLeCam/react-leaflet/issues/453
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
+
+const Map = ({ jobOffers, centerXy }) => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height: 400, width: "100%"}}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: 400, width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
       <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+        <Popup>Popup Here</Popup>
       </Marker>
     </MapContainer>
   )
